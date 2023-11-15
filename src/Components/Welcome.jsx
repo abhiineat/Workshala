242915d841fc12bdebd2c346cd8957fbcc7901a6
abhinav2210103 <abhinav2210103@akgec.ac.in>
2023-11-15 21:50:08 +0530

    responsive to different views

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -7,41 +7,45 @@ function Welcome() {
     <>
     <Navbar/>
     <div className="bg-[#FFAEB6] bg-opacity-[12%] h-[58rem] ">
-      <div className="flex mt-10">
-        <div className="ml-5 mt-10 "><img src={welcome}/></div>
-        <div className="bg-white h-[55rem] w-full mt-11  ml-8 font-sans">
-            <div className="mt-5 ml-10">
-           <div className="text-[3rem] font-600">Welcome</div>
-           <div className="bg-[#FFAEB6] bg-opacity-[20%] flex justify-center w-1/2 rounded-xl mt-5"><div>Your account is created successfully. Let's get started</div></div>
-           <div className="text-[1.5rem]">Work Status</div>
-           <div className=" flex">
-            <div className="border-2">
-                <div>I'm Experienced</div>
-                <div>I have Work Experience <br></br> (Excluding Internships)</div>
+      <div className="flex mt-10 flex-wrap md:flex-nowrap">
+        <div className="md:ml-5 md:mt-10 m-0 "><img src={welcome}/></div>
+        <div className="bg-white h-[55rem] w-full mt-11  md:ml-8 ml-0 font-sans">
+            <div className="mt-5 md:ml-10 ml-0 ">
+           <div className="text-[3rem] font-600 flex justify-center md:justify-start">Welcome</div>
+           <div className="bg-[#FFAEB6] bg-opacity-[20%]  justify-center w-1/2 rounded-xl mt-5 hidden md:flex "><div>Your account is created successfully. Let's get started</div></div>
+           <div className="text-[1.5rem] mt-8 font-500 flex justify-center md:justify-start">Work Status</div>
+           <div className=" flex justify-evenly flex-wrap mt-5 ">
+            <div className="border-2 border-black rounded-xl justify-center">
+                <div className="md:py-2 md:pr-12 md:pl-8 p-1">
+                <div className="text-2xl md:m-0">I'm Experienced</div>
+                <div className="text-sm mt-3">I have Work Experience <br></br> ( Excluding Internships)</div>
+                </div>
             </div>
-            <div className="border-2">
-             <div>I'm Fresher</div>
-             <div>I am a Student <br></br>Haven't Worked after Graduation</div>
-
+            <div className="border-2 border-black rounded-xl mt-5 md:m-0">
+                <div className="md:py-2 md:px-8 p-1 ">
+             <div className="text-2xl ">I'm Fresher</div>
+             <div className="text-sm mt-3">I am a Student <br></br>Haven't Worked after Graduation</div>
+             </div>
             </div>
            </div>
-           <div>Your Skills</div>
-           <div className="flex justify-evenly">
-            <div>Finance</div>
-            <div>Architecture</div>
-            <div>onsultancy</div>
+           <div className="text-[1.5rem] mt-8 font-500 flex justify-center md:justify-start" >Your Skills</div>
+           <div className="flex justify-evenly flex-wrap">
+            <button className="bg-[#DEC1FF] py-1 bg-opacity-50 px-6 rounded text-xl mt-5 md:m-0" >Finance</button>
+            <button className="bg-[#DEC1FF] py-1 bg-opacity-50 px-6 rounded text-xl mt-5 md:m-0" >Architecture</button>
+            <button className="bg-[#DEC1FF] py-1 bg-opacity-50 px-6 rounded text-xl mt-5 md:m-0" >Consultancy</button>
+           
            </div>
-           <div className="flex justify-evenly">
-            <div>Design</div>
-            <div>IT and Software</div>
-            <div>Database</div>
+           <div className="flex justify-evenly mt-5 flex-wrap">
+           <button className="bg-[#DEC1FF] py-1 bg-opacity-50 px-6 rounded text-xl ml-4" >Design</button>
+            <button className="bg-[#DEC1FF] py-1 bg-opacity-50 px-10  rounded text-xl mt-5 md:m-0" >IT and Software</button>
+            <button className="bg-[#DEC1FF] py-1 bg-opacity-50 px-8 rounded text-xl mt-5 md:m-0" >Database</button>
            </div>
-           <div className="flex justify-evenly"> 
-            <div>Marketing</div>
-            <div>Writing</div>
-            <div>Web Development</div>
+           <div className="flex justify-evenly mt-5 flex-wrap"> 
+           <button className="bg-[#DEC1FF] py-1 bg-opacity-50 px-6 rounded text-xl ml-5 mt-5 md:m-0" >Marketing</button>
+            <button className="bg-[#DEC1FF] py-1 bg-opacity-50 px-6 rounded text-xl mt-5 md:m-0" >Writing</button>
+            <button className="bg-[#DEC1FF] py-1 bg-opacity-50 px-6 rounded text-xl mr-4 mt-5 md:m-0" >Web Development</button>
            </div>
-           <div>Submit</div>
+           <div className="border-2 border-customColor flex w-40 justify-center  ml-16 md:ml-40 mt-10 rounded-xl "><div className="py-1 px-10 text-xl" >Submit</div></div>
 
         </div>
         </div>
