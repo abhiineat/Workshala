8cce8e96b3197e9d995eb7064b6f17be5deafa18
abhinav2210103 <abhinav2210103@akgec.ac.in>
2023-11-16 02:11:21 +0530

    merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import Companies from './Components/Companies.jsx'
 import Jobs from './Components/Jobs.jsx'
 import Profile from './Components/Profile.jsx'
 import Welcome from './Components/Welcome.jsx'
+import Home from './Home.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
    <>
-   <Route path='/' element={<App />} />
+   <Route path='/' element={<Home />} />
    <Route path='/companies' element={<Companies/>}/>
    <Route path='/jobs' element={<Jobs/>}/>
    <Route path='/profile' element={<Profile/>}/>
