05ed3c6bd794f1cc6b2f3df08e0f1c08021ba6ef
abhineet <abhii.neat@gmail.com>
2023-11-17 14:50:50 +0530

    changes for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-toastify': 'react-toastify/dist/index.js',
-    },
-  },
-})
-export default {
   base: 'https://workshala-eight.vercel.app'
-};
+})
